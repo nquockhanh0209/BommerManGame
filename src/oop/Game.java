8c7d5cb79cd4b41b60aa5cb35eca4323e6740b93
nquockhanh0209 <60789588+nquockhanh0209@users.noreply.github.com>
2020-12-09 03:45:35 +0700

    Update Game.java

--- src/oop/Game.java
+++ src/oop/Game.java
@@ -1,6 +1,8 @@
 package oop;
 
 import oop.control.Control;
+import oop.enemy.Ballom;
+import oop.enemy.Oneal;
 import oop.entities.*;
 import oop.graphics.Sprite;
 import javafx.scene.Scene;
@@ -20,7 +22,7 @@ import java.util.List;
 
 public class Game {
     public static final int WIDTH = 30;
-    public static final int HEIGHT = 15;
+    public static final int HEIGHT = 13;
 
     private GraphicsContext gc;
     private Canvas canvas;
@@ -49,7 +51,8 @@ public class Game {
     }
 
     public void createMap() throws Exception {
-        File file = new File("res/textures/level1.txt");
+        //File file = new File("res/textures/level1.txt");
+        File file = new File("res/textures/test");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String string;
         int h = 0;
@@ -61,9 +64,11 @@ public class Game {
                 switch (block) {
                     case '1':
                         stillObjects.add(new Wall(i, h, Sprite.wall.getFxImage()));
+                        bricks.add(new Grass(i, h, Sprite.grass.getFxImage()));
                         break;
                     case '0':
                         stillObjects.add(new Grass(i, h, Sprite.grass.getFxImage()));
+                        bricks.add(new Grass(i, h, Sprite.grass.getFxImage()));
                         break;
                     case '2':
                         stillObjects.add(new Grass(i, h, Sprite.grass.getFxImage()));
@@ -71,15 +76,29 @@ public class Game {
                         break;
                     case '3':
                         stillObjects.add(new Grass(i, h, Sprite.grass.getFxImage()));
+                        bricks.add(new Grass(i, h, Sprite.grass.getFxImage()));
                         items.add(new ItemS(i, h, Sprite.brick.getFxImage()));
                         break;
                     case '4':
                         stillObjects.add(new Grass(i, h, Sprite.grass.getFxImage()));
                         items.add(new ItemB(i, h, Sprite.brick.getFxImage()));
+                        bricks.add(new Grass(i, h, Sprite.grass.getFxImage()));
                         break;
                     case '5':
                         stillObjects.add(new Grass(i, h, Sprite.grass.getFxImage()));
                         items.add(new ItemF(i, h, Sprite.brick.getFxImage()));
+                        bricks.add(new Grass(i, h, Sprite.grass.getFxImage()));
+                        break;
+                    case 'b':
+                        stillObjects.add(new Grass(i, h, Sprite.grass.getFxImage()));
+                        entities.add(new Ballom(i, h, Sprite.balloom_right1.getFxImage()));
+                        bricks.add(new Grass(i, h, Sprite.grass.getFxImage()));
+                        System.out.println("ok");
+                        break;
+                    case 'o':
+                        stillObjects.add(new Grass(i, h, Sprite.grass.getFxImage()));
+                        entities.add(new Oneal(i, h, Sprite.oneal_right1.getFxImage()));
+                        bricks.add(new Grass(i, h, Sprite.grass.getFxImage()));
                         break;
                 }
             }
@@ -90,15 +109,9 @@ public class Game {
     public void update() {
         if(input.isPlacebomb() && maxBomb >= 1 && !press) {
             Entity bomb;
-            if(bomber.getY()/Sprite.SCALED_SIZE == 12) {
-                int y = bomber.getY()/Sprite.SCALED_SIZE+1;
-                bomb = new Bomb(bomber.getX()/Sprite.SCALED_SIZE, y, Sprite.bomb.getFxImage());
-            }
-            else {
-                int x = (bomber.getX() + 15) /Sprite.SCALED_SIZE;
-                int y = (bomber.getY() + 20) /Sprite.SCALED_SIZE;
-                bomb = new Bomb(x, y, Sprite.bomb.getFxImage());
-            }
+            int x = (bomber.getX() + 15) /Sprite.SCALED_SIZE;
+            int y = (bomber.getY() + 20) /Sprite.SCALED_SIZE;
+            bomb = new Bomb(x, y, Sprite.bomb.getFxImage());
             bombs.add(bomb);
             maxBomb-=1;
             press = true;
@@ -107,8 +120,11 @@ public class Game {
         else if(!input.isPlacebomb()) {
             press = false;
         }
-        for(Entity e : entities) {
-            e.update();
+        for(int i = 0; i < bricks.size(); i++) {
+            bricks.get(i).update();
+        }
+        for(int i = 0; i < entities.size(); i++) {
+            entities.get(i).update();
         }
         //for(Entity e : flames) {
            // e.update();
@@ -130,18 +146,27 @@ public class Game {
         }
         for(int i = 0; i < bricks.size(); i++) {
             for(int j = 0; j < flames.size(); j++) {
-                if(flames.get(j).collide(bricks.get(i))) {
-                    //bricks.remove(bricks.get(i));
-                    ((Brick) bricks.get(i)).isDestroy = true;
-                    break;
+                if(bricks.get(i) instanceof Brick) {
+                    if(flames.get(j).collide(bricks.get(i))) {
+                        //bricks.remove(bricks.get(i));
+                        ((Brick) bricks.get(i)).isDestroy = true;
+                        break;
+                    }
                 }
             }
         }
         for(int i = 0; i < entities.size(); i++) {
             for(int j = 0; j < flames.size(); j++) {
                 if(flames.get(j).collide(entities.get(i))) {
-                    entities.remove(entities.get(i));
-                    break;
+                    if(entities.get(i) instanceof Oneal) {
+                        ((Oneal) entities.get(i)).isDestroy = true;
+                    }
+                    else if(entities.get(i) instanceof Ballom) {
+                        ((Ballom) entities.get(i)).isDestroy = true;
+                    }
+                    else if(entities.get(i) instanceof Bomber) {
+                        ((Bomber) entities.get(i)).isDestroy = true;
+                    }
                 }
             }
         }
@@ -165,19 +190,21 @@ public class Game {
         for(int i = 0; i < items.size(); i++) {
             items.get(i).update();
         }
-        for(int i = 0; i < bricks.size(); i++) {
-            bricks.get(i).update();
-        }
     }
 
     public void render() {
+       // System.out.println(getBrickType(7, 1) + " " + getBlockType(7, 1));
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         stillObjects.forEach(g -> g.render(gc));
-        bombs.forEach(g -> g.render(gc));
-        flames.forEach(g -> g.render(gc));
+        for(Entity b : bricks) {
+            if(!(b instanceof Grass)){
+                b.render(gc);
+            }
+        }
         items.forEach(g -> g.render(gc));
         entities.forEach(g -> g.render(gc));
-        bricks.forEach(g -> g.render(gc));
+        bombs.forEach(g -> g.render(gc));
+        flames.forEach(g -> g.render(gc));
 
     }
 
@@ -195,4 +222,16 @@ public class Game {
         int i = x + (WIDTH*y);
         return stillObjects.get(i).getClass().toString();
     }
+
+    public static String getBrickType(int x, int y) {
+        String type = "";
+        int i = x + (WIDTH*y);
+        return bricks.get(i).getClass().toString();
+    }
+
+    public static Entity getBrick(int x, int y) {
+        int i = x + (WIDTH*y);
+        return (Brick)bricks.get(i);
+    }
+
 }
